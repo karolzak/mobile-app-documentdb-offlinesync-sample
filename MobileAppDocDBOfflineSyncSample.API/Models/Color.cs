3a14980eff46c108e701034c8876f9970804724c
Dibran Mulder <dibran.mulder@outlook.com>
2017-01-11 12:49:20 +0100

    Moved the actual doc db models to the api instead of the pcl

---
+++ MobileAppDocDBOfflineSyncSample.API/Models/Color.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Mobile.Server.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MobileAppDocDBOfflineSyncSampleService.Models
+{
+    public class Color : Document, ITableData
+    {
+        public string Name { get; set; }
+        public string Business { get; set; }
+        public string Market { get; set; }
+        public List<Code> Codes { get; set; }
+
+        public byte[] Version { get; set; }
+
+        public DateTimeOffset? CreatedAt { get; set; }
+
+        public DateTimeOffset? UpdatedAt { get; set; }
+
+        public bool Deleted { get; set; }
+    }
+}
---
+++ MobileAppDocDBOfflineSyncSample.API/Models/PaintSystem.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Mobile.Server.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileAppDocDBOfflineSyncSampleService.Models
+{
+    public class PaintSystem : Document, ITableData
+    {
+        public DateTime FormulaCreationDate { get; set; }
+
+        public List<FormulaComponent> Components { get; set; }
+
+        public string State { get; set; }
+
+        public byte[] Version { get; set; }
+
+        public DateTimeOffset? CreatedAt { get; set; }
+
+        public DateTimeOffset? UpdatedAt { get; set; }
+
+        public bool Deleted { get; set; }
+    }
+}
